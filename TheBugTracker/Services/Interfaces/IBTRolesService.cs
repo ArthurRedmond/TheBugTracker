3881c22973a7bf75a36ecc3cc7027f942640cfae
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-04-05 10:49:05 -0400

    Created RolesService Interface and implementation

---
+++ TheBugTracker/Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRolesAync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+    }
+}
