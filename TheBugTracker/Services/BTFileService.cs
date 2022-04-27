78f508ecbf0840ba2035b9d4bfc584887a8b7712
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-04-27 17:26:27 -0400

    Create File Service class

---
+++ TheBugTracker/Services/BTFileService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTFileService : IBTFileService
+    {
+        private readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            try
+            {
+                string imageBase64Data = Convert.ToBase64String(fileData); 
+                return string.Format($"data:{extension};base64,{imageBase64Data}");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            try
+            {
+                MemoryStream memoryStream = new();
+                await file.CopyToAsync(memoryStream);
+                byte[] byteArray = memoryStream.ToArray();
+                memoryStream.Close();
+                memoryStream.Dispose();
+
+                return byteArray;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public string FormatFileSize(long bytes)
+        {
+            int counter = 0;
+            decimal fileSize = bytes;
+            while (Math.Round(fileSize / 1024) >= 1)
+            {
+                fileSize /= bytes;
+                counter++;
+            }
+            return String.Format("{0:n1}{1}",fileSize,suffixes[counter]);
+        }
+
+        public string GetFileIcon(string file)
+        {
+            string fileImage = "default";
+
+            if (!string.IsNullOrWhiteSpace(file))
+            {
+                fileImage = Path.GetExtension(file).Replace(".", "");
+                return $"/img/png/{fileImage}.png";
+            }
+            return fileImage; 
+        }
+    }
+}
