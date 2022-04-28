5d58c26073db393b9de1a81c131b3c3f5d20b206
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-04-28 16:10:18 -0400

    Create new enums

---
+++ TheBugTracker/Models/Enums/BTTIcketType.cs
@@ -0,0 +1,12 @@
+﻿namespace TheBugTracker.Models.Enums
+{
+    public enum BTTIcketType
+    {
+        NewDevelopment,
+        WorkTask,
+        Defect,
+        ChangeRequest,
+        Enhancement,
+        GeneralTask
+    }
+}
