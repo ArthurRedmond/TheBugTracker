2165063496fbcdac110c76938b97569aff8b4840
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-04-29 16:39:56 -0400

    Added data migration for changes to property names

---
+++ TheBugTracker/Data/Migrations/20220429203650_Updates_02.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheBugTracker.Data.Migrations
+{
+    public partial class Updates_02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_TicketPriorities_TicketPriorityId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "TicketPropertyId",
+                table: "Tickets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketPriorityId",
+                table: "Tickets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_TicketPriorities_TicketPriorityId",
+                table: "Tickets",
+                column: "TicketPriorityId",
+                principalTable: "TicketPriorities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_TicketPriorities_TicketPriorityId",
+                table: "Tickets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketPriorityId",
+                table: "Tickets",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicketPropertyId",
+                table: "Tickets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_TicketPriorities_TicketPriorityId",
+                table: "Tickets",
+                column: "TicketPriorityId",
+                principalTable: "TicketPriorities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
