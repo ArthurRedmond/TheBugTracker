0ae25619e84c7cc6e2eb9875336a726d3f85be79
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-05-05 12:00:26 -0400

    Created user claim

---
+++ TheBugTracker/Extensions/IdentityExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace TheBugTracker.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static int? GetCompanyId(this IIdentity identity)
+        {
+            Claim claim = ((ClaimsIdentity)identity).FindFirst("CompanyId");
+            // Ternary operator (if/else)
+            return (claim != null) ? int.Parse(claim.Value) : null;
+        }
+    }
+}
