418ab5933fe0c9aad50ea227c136f294779ac002
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-06-24 16:07:21 -0400

    Create User Role controller

---
+++ TheBugTracker/Controllers/UserRolesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBugTracker.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
