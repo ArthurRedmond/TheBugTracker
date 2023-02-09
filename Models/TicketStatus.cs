2708d83279fd093cfcd04646ce5ba1a0256a78ab
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-02-09 16:42:10 -0500

    created ticket type, priority, status, and history models.

---
+++ Models/TicketStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class TicketStatus
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Status Name")]
+        public string Name { get; set; }
+    }
+}
