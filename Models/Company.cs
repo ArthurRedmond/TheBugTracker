58cb21070778ff7c945a8d78ec0251d18b0f9b56
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-02-09 20:54:33 -0500

    created remaining needed models

---
+++ Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Company Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        // Navigation properties
+
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+
+    }
+}
---
+++ Models/Notification.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Ticket")]
+        public int TicketId { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [Required]
+        [Display(Name = "Recipient")]
+        public string RecipientId { get; set; }
+
+        [Required]
+        [Display(Name = "Sender")]
+        public string SenderId { get; set; }
+
+        [Display(Name = "Has been viewed")]
+        public bool Viewed { get; set; }
+
+        // Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser Recipient { get; set; }
+        public virtual BTUser Sender { get; set; }
+
+    }
+}
