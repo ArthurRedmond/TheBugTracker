f9b9a6ec4e92a5052772ac09edd84eb7799442c4
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-02-13 21:48:53 -0500

    mend

--- Services/Interfaces/IBTRolesService.cs
+++ Services/Interfaces/IBTRolesService.cs
@@ -1,6 +1,23 @@
-﻿namespace TheBugTracker.Services.Interfaces
+﻿using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
 {
     public interface IBTRolesService
     {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUsertoRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserfromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+        
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
     }
 }
