62cd77b8ab7269566bd5a8abd62952e69dd0c504
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-03-24 08:59:07 -0400

    Created mailsettings class and service

---
+++ Models/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace TheBugTracker.Models
+{
+    public class MailSettings
+    {
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string MailPassword { get; set; }
+        public string MailHost { get; set; }
+        public int MailPort { get; set; }
+    }
+}
